f57b6c37be49f8d2248982a845522c109af2ec1f
ledat0711 <leanhdat1994@gmail.com>
2022-08-01 17:16:32 +0700

    Send

--- ss01_introduction_to_java/bai_tap/src/ChangeMoney.java
+++ ss01_introduction_to_java/bai_tap/src/ChangeMoney.java
@@ -4,7 +4,7 @@ public class ChangeMoney {
     public static void main(String[] args) {
         double exchangeRate = 23000;
         double usdInput;
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the amount in USD: ");
         usdInput = sc.nextDouble();
         double vndOutput = usdInput * exchangeRate;
