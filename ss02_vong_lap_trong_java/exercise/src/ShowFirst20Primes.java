ee643a856f2832986b0e9d7ec526955cff025c4a
ledat0711 <leanhdat1994@gmail.com>
2022-08-02 13:36:42 +0700

    Send

--- ss02_vong_lap_trong_java/exercise/src/ShowFirst20Primes.java
+++ ss02_vong_lap_trong_java/exercise/src/ShowFirst20Primes.java
@@ -1,8 +1,23 @@
-public class ShowFirst20Primes {
-    public static void checkPrime(int numberFirst){
-        int i;
-        for(i=0; i<=Math.sqrt(numberFirst); i++){
+import java.util.Scanner;
 
+public class ShowFirst20Primes {
+    public static boolean checkPrime(int numberInside) {
+        int i;
+        for (i = 2; i <= Math.sqrt(numberInside); i++) {
+            if (numberInside % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 2; count != 20; i++) {
+            if (checkPrime(i)) {
+                System.out.print(i + "\t");
+                count++;
+            }
         }
     }
 }
