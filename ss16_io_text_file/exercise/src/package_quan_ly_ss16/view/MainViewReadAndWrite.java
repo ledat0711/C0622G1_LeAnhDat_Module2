36558f0154a9cedf131d524f9fbcc1b327059c91
ledat0711 <leanhdat1994@gmail.com>
2022-08-22 21:11:52 +0700

    Send

--- ss16_io_text_file/exercise/src/package_quan_ly_ss16/view/MainViewReadAndWrite.java
+++ ss16_io_text_file/exercise/src/package_quan_ly_ss16/view/MainViewReadAndWrite.java
@@ -1,25 +1,16 @@
-/*Mô tả bài toán
-Viết một chương trình cho phép người dùng sao chép các tập tin.
-Người sử dụng cần phải cung cấp một tập tin nguồn (source file) và một tập tin đích (target file).
-
-Chương trình sao chép tệp nguồn vào tệp tin đích và hiển thị số ký tự trong tệp.
-Chương trình nên cảnh báo người dùng nếu tệp nguồn không tồn tại hoặc nếu tập tin đích đã tồn tại.
-
-Để sao chép nội dung từ tệp tin nguồn đến tệp tin đích,
-bạn nên sử dụng luồng đầu vào để đọc ký tự từ tệp nguồn và luồng đầu ra để gửi byte đến tệp tin đích,
-bất kể nội dung của tệp.*/
-package copy_file_text;
+package package_quan_ly_ss16.view;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainView {
+public class MainViewReadAndWrite {
     public static void main(String[] args) throws IOException {
         add();
     }
 
     private static void writeFile(List<String> strings) throws IOException {
-        File file = new File("src\\copy_file_text\\data\\destination_file.csv");
+        File file = new File("src\\package_quan_ly_ss16\\data\\destination_file.csv");
         FileWriter fileWriter = new FileWriter(file);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         for (String str : strings) {
@@ -34,7 +25,7 @@ public class MainView {
         writeFile(strings);
     }
     private static List<String> readFile() throws IOException {
-        File file = new File("src\\copy_file_text\\data\\source.csv");
+        File file = new File("src\\package_quan_ly_ss16\\data\\source_file.csv");
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         List<String> stringAndLengths = new ArrayList<>();
